417531ff7c4c3b58ac881c8f25aeec2ad82e6889
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-11-23 14:32:58 +0200

    Updated api paths
    Updated api paths to be more clear what they are doing

--- routes/api.php
+++ routes/api.php
@@ -19,23 +19,23 @@ Route::group(['middleware' => ['auth:sanctum']],function (){
 
     /** -----User routes----- */
     Route::get('profile',[UserController::class,'index']);
-    Route::patch('profile',[UserController::class,'update']);
-    Route::delete('profile',[UserController::class,'destroy']);
+    Route::patch('profile/update',[UserController::class,'update']);
+    Route::delete('profile/delete',[UserController::class,'destroy']);
     Route::post('logout',[AuthController::class,'logout']);
 
     /** -----GOALS routes----- */
     Route::get('goals',[GoalController::class,'index']);
     Route::get('goals/{id}',[GoalController::class,'show']);
     Route::post('goals/add',[GoalController::class,'store']);
-    Route::patch('goals/{id}',[GoalController::class,'update']);
-    Route::delete('goals/{id}',[GoalController::class,'destroy']);
+    Route::patch('goals/update/{id}',[GoalController::class,'update']);
+    Route::delete('goals/delete/{id}',[GoalController::class,'destroy']);
 
     /** -----TASKS routes----- */
     Route::get('tasks',[TaskController::class,'index']);
     Route::get('tasks/{id}',[TaskController::class,'show']);
     Route::post('tasks/add',[TaskController::class,'store']);
-    Route::patch('tasks/{id}',[TaskController::class,'update']);
-    Route::delete('tasks/{id}',[TaskController::class,'destroy']);
+    Route::patch('tasks/update/{id}',[TaskController::class,'update']);
+    Route::delete('tasks/delete{id}',[TaskController::class,'destroy']);
 
 });
 
